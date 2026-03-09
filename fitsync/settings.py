1d849193a8a65fd444c9750c2d8a922856e66720
Adithyan K <adithyankeloth2244@gmail.com>
2026-03-09 22:49:46 +0530

    Auto-switch to SQLite on Vercel to prevent connection errors

--- fitsync/settings.py
+++ fitsync/settings.py
@@ -76,16 +76,26 @@ WSGI_APPLICATION = 'fitsync.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fitsync_db',
-        'USER': 'root',
-        'PASSWORD': 'root',  # Enter your MySQL password here
-        'HOST': 'localhost',
-        'PORT': '3306',
+import os
+
+if 'VERCEL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'fitsync_db',
+            'USER': 'root',
+            'PASSWORD': 'root',  # Enter your MySQL password here
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
 
 # Original SQLite Config (Commented out)
 # DATABASES = {
