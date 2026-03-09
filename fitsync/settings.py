0a9851272ce6aed939dae561d3a10890ab833a5a
Adithyan K <adithyankeloth2244@gmail.com>
2026-03-09 22:55:28 +0530

    Fix Vercel database crash by using in-memory SQLite

--- fitsync/settings.py
+++ fitsync/settings.py
@@ -82,7 +82,7 @@ if 'VERCEL' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': ':memory:',
         }
     }
 else:
